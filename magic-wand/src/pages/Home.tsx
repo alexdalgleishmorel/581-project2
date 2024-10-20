7caf09a04e2b675b25dbce377855d85fb8cf143d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-10-20 14:50:53 -0600

    10 seconds of sequence detection

--- magic-wand/src/pages/Home.tsx
+++ magic-wand/src/pages/Home.tsx
@@ -1,10 +1,25 @@
-
+import React, { useState } from 'react';
 import { IonButton } from '@ionic/react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
-
 const Home: React.FC = () => {
+  const [isInSequenceDetectionState, setIsInSequenceDetectionState] = useState(false);
+
+  const detectUnlockSequence = () => {
+  };
+
+  const handleUnlock = () => {
+    setIsInSequenceDetectionState(true);
+
+    detectUnlockSequence();
+
+    // Exiting sequence detection state after 10 seconds
+    setTimeout(() => {
+      setIsInSequenceDetectionState(false);
+    }, 10000);
+  };
+
   return (
     <div>
       <div className="title">
@@ -13,12 +28,16 @@ const Home: React.FC = () => {
         <p className="line">Wish</p>
       </div>
       <div className='editHome'>
-        <Link to="/edit" style={{ textDecoration: 'none' }}>
-          <IonButton>Edit</IonButton>
-        </Link>
+        {!isInSequenceDetectionState && (
+          <>
+            <IonButton onClick={handleUnlock}>Unlock</IonButton>
+            <Link to="/edit" style={{ textDecoration: 'none' }}>
+              <IonButton>Edit</IonButton>
+            </Link>
+          </>
+        )}
       </div>
 
-      
       <div className='body'>
         {/* Functionality to detect the sensors */}
       </div>
